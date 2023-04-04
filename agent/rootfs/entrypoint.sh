f1fe11707555032a066a61841cf34f7187f729c8
SeanLy <seanly@opsbox.dev>
2023-04-04 14:06:55 +0000

    merge agent images

---
+++ agent/rootfs/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p /var/log/supervisor/ 
+
+_agentService=${AGENT_SERVICE:-jenkins-agent}
+
+if [ -f /etc/supervisor/conf.d/${_agentService}.conf.disable ]; then
+  mv /etc/supervisor/conf.d/${_agentService}.conf.disable /etc/supervisor/conf.d/${_agentService}.conf
+fi
+
+_cleanupPeriodic=${CLEANUP_PERIODIC:-daily}
+
+if [ -f /usr/local/bin/jenkins-clean-data ]; then
+  ln -s /usr/local/bin/jenkins-clean-data /etc/periodic/${_cleanupPeriodic}/jenkins-clean-data
+fi
+
+# start superviosrd
+supervisord --nodaemon --configuration /etc/supervisord.conf
